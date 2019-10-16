493b99454956c1dcc3fb57a91a3c77a24db1800f
Hiflan <hiflan.2015@gmail.com>
2019-10-16 23:08:45 +0530

    sign up added

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.util';
+import { auth, signInWithGoogle } from '../../firebase/firebase.util';
 
 import './sign-in.styles.scss';
+
 
 class SignIn extends React.Component{
     constructor(props){
@@ -17,10 +18,18 @@ class SignIn extends React.Component{
             password:''
         }   
     }
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({email:'', password:''})
+        const {email, password} = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'', password:''})
+        } catch( error){
+            console.log(error);
+        }
+        
     }
 
     handleChange = event => {
